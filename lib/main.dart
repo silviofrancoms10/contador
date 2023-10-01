30e1ae761a649a017e18924f5bf33d5b54675837
silviofrancoms <silviofrancoms@outlook.com>
2023-10-01 08:28:11 -0400

    Aula 76 - Personalizando o Botao e Padding

--- lib/main.dart
+++ lib/main.dart
@@ -24,7 +24,7 @@ class HomePage extends StatelessWidget {
   }
 
   void increment() {
-    print("Increment");
+    print('Increment');
   }
 
   @override
@@ -32,11 +32,12 @@ class HomePage extends StatelessWidget {
     return Scaffold(
       //Widget andaime (modelo para você criar a sua página)
       backgroundColor: Colors.black,
-      appBar: AppBar(  //BARRA
+      appBar: AppBar(
+        //BARRA
         title: Text("Assistido!"),
         backgroundColor: Colors.green,
       ),
-      drawer: Drawer(),  //MENU
+      drawer: Drawer(), //MENU
       body: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         //Alinhamento do eixo principal
@@ -49,18 +50,31 @@ class HomePage extends StatelessWidget {
                 fontWeight: FontWeight.w700,
                 letterSpacing: 2,
               )),
-          Text(
-            '0',
-            style: TextStyle(
-              fontSize: 100,
-              color: Colors.white,
+          //const SizedBox(height: 100),
+          const Padding(
+            padding: EdgeInsets.all(180),
+            child: Text(
+              '0',
+              style: TextStyle(
+                fontSize: 100,
+                color: Colors.white,
+              ),
             ),
           ),
+          //const SizedBox(height: 32),
           Row(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
               TextButton(
                 onPressed: decrement,
+                style: TextButton.styleFrom(
+                    backgroundColor: Colors.white,
+                    //padding: const EdgeInsets.all(32),
+                    fixedSize: const Size(100, 100),
+                    primary: Colors.red,
+                    shape: RoundedRectangleBorder(
+                        side: BorderSide(color: Colors.green, width: 5),
+                        borderRadius: BorderRadius.circular(24))),
                 child: Text(
                   "Saiu",
                   style: TextStyle(
@@ -68,8 +82,17 @@ class HomePage extends StatelessWidget {
                   ),
                 ),
               ),
+              const SizedBox(width: 32),
               TextButton(
                 onPressed: increment,
+                style: TextButton.styleFrom(
+                    backgroundColor: Colors.white,
+                    // padding: const EdgeInsets.all(32),
+                    fixedSize: const Size(100, 100),
+                    primary: Colors.red,
+                    shape: RoundedRectangleBorder(
+                        side: BorderSide(color: Colors.green, width: 5),
+                        borderRadius: BorderRadius.circular(24))),
                 child: Text(
                   "Entrou",
                   style: TextStyle(
