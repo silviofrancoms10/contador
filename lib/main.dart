0878a366b25ae9af0df77980010124b2f9b16ae4
silviofrancoms <silviofrancoms@outlook.com>
2023-10-01 10:17:02 -0400

    Aula 77 - Imagem de fundo, removendo banner debug

--- lib/main.dart
+++ lib/main.dart
@@ -10,21 +10,25 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
+      debugShowCheckedModeBanner: false, // remover a faixa de debug no app
       home: HomePage(),
     );
   }
 }
 
 class HomePage extends StatelessWidget {
-  const HomePage({super.key});
+  HomePage({super.key});
 
+  int count = 0;
   void decrement() {
-    print('Decrement');
+    count--;
+    print(count);
   }
 
   void increment() {
-    print('Increment');
+    count++;
+    print(count);
   }
 
   @override
@@ -37,72 +41,82 @@ class HomePage extends StatelessWidget {
         title: Text("Assistido!"),
         backgroundColor: Colors.green,
       ),
-      drawer: Drawer(), //MENU
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        //Alinhamento do eixo principal
+      drawer: Drawer(),
+      //MENU
+      body: Container(
+        decoration: const BoxDecoration(
+            image: DecorationImage(
+              image: AssetImage('assets/images/backgroundapp.jpg'),
+              fit: BoxFit.cover,
+              // image: NetworkImage('https://img.freepik.com/fotos-gratis/uma-imagem-de-um-planeta-com-um-buraco-negro-no-centro-e-um-buraco-negro-no-centro_1340-23795.jpg?w=1380&t=st=1696166756~exp=1696167356~hmac=60ff9fb392ff9f9da1d6c1debd02bf3ccf9135e4778c924df5841f645aba094a'),
+                ),
+        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          //Alinhamento do eixo principal
 
-        children: [
-          Text('Pode entrar!',
-              style: TextStyle(
-                fontSize: 34,
-                color: Colors.white,
-                fontWeight: FontWeight.w700,
-                letterSpacing: 2,
-              )),
-          //const SizedBox(height: 100),
-          const Padding(
-            padding: EdgeInsets.all(180),
-            child: Text(
-              '0',
-              style: TextStyle(
-                fontSize: 100,
-                color: Colors.white,
+          children: [
+            Text('Pode entrar!',
+                style: TextStyle(
+                  fontSize: 34,
+                  color: Colors.white,
+                  fontWeight: FontWeight.w700,
+                  letterSpacing: 2,
+                )),
+            //const SizedBox(height: 100),
+            const Padding(
+              padding: EdgeInsets.all(180),
+              child: Text(
+                '0',
+                style: TextStyle(
+                  fontSize: 100,
+                  color: Colors.white,
+                ),
               ),
             ),
-          ),
-          //const SizedBox(height: 32),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              TextButton(
-                onPressed: decrement,
-                style: TextButton.styleFrom(
-                    backgroundColor: Colors.white,
-                    //padding: const EdgeInsets.all(32),
-                    fixedSize: const Size(100, 100),
-                    primary: Colors.red,
-                    shape: RoundedRectangleBorder(
-                        side: BorderSide(color: Colors.green, width: 5),
-                        borderRadius: BorderRadius.circular(24))),
-                child: Text(
-                  "Saiu",
-                  style: TextStyle(
-                    color: Colors.red,
+            //const SizedBox(height: 32),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                TextButton(
+                  onPressed: decrement,
+                  style: TextButton.styleFrom(
+                      backgroundColor: Colors.white,
+                      //padding: const EdgeInsets.all(32),
+                      fixedSize: const Size(100, 100),
+                      primary: Colors.red,
+                      shape: RoundedRectangleBorder(
+                          side: BorderSide(color: Colors.green, width: 5),
+                          borderRadius: BorderRadius.circular(24))),
+                  child: Text(
+                    "Saiu",
+                    style: TextStyle(
+                      color: Colors.red,
+                    ),
                   ),
                 ),
-              ),
-              const SizedBox(width: 32),
-              TextButton(
-                onPressed: increment,
-                style: TextButton.styleFrom(
-                    backgroundColor: Colors.white,
-                    // padding: const EdgeInsets.all(32),
-                    fixedSize: const Size(100, 100),
-                    primary: Colors.red,
-                    shape: RoundedRectangleBorder(
-                        side: BorderSide(color: Colors.green, width: 5),
-                        borderRadius: BorderRadius.circular(24))),
-                child: Text(
-                  "Entrou",
-                  style: TextStyle(
-                    color: Colors.red,
+                const SizedBox(width: 32),
+                TextButton(
+                  onPressed: increment,
+                  style: TextButton.styleFrom(
+                      backgroundColor: Colors.white,
+                      // padding: const EdgeInsets.all(32),
+                      fixedSize: const Size(100, 100),
+                      primary: Colors.red,
+                      shape: RoundedRectangleBorder(
+                          side: BorderSide(color: Colors.green, width: 5),
+                          borderRadius: BorderRadius.circular(24))),
+                  child: Text(
+                    "Entrou",
+                    style: TextStyle(
+                      color: Colors.red,
+                    ),
                   ),
-                ),
-              )
-            ],
-          )
-        ],
+                )
+              ],
+            )
+          ],
+        ),
       ),
     );
   }
